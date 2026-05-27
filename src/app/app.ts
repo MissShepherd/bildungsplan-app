4f353a82c797000ba252602973bafa74adfe75de
tatjana.schaefer <tatjana.schaefer@santismail.ch>
2026-05-27 13:42:12 +0200

    feat: update app layout and styles, enhance navigation structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
 import { FilterSidebarComponent } from './features/filter-sidebar/filter-sidebar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, FilterSidebarComponent],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    FilterSidebarComponent,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App {
+  readonly title = 'bildungsplan-frontend';
+}
