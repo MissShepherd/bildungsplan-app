a5054edf6b396aff1556cf70d5f5daeab73c907e
tatjana.schaefer <tatjana.schaefer@santismail.ch>
2026-05-28 13:01:41 +0200

    refactor: reorganize imports and improve code formatting in bildungsplan-context.service.ts

--- src/app/core/services/bildungsplan-context.service.ts
+++ src/app/core/services/bildungsplan-context.service.ts
@@ -1,14 +1,14 @@
 import { inject, Injectable } from '@angular/core';
 import { forkJoin, Observable } from 'rxjs';
 
+import { Lernort } from '../../models/lernort.model';
 import { Handlungskompetenzbereich } from '../../models/handlungskompetenzbereich.model';
 import { Handlungskompetenz } from '../../models/handlungskompetenz.model';
 import { Modul } from '../../models/modul.model';
 import { HandlungskompetenzbereichService } from './handlungskompetenzbereich.service';
 import { HandlungskompetenzService } from './handlungskompetenz.service';
-import { ModulService } from './modul.service';
-import { Lernort } from '../../models/lernort.model';
 import { LernortService } from './lernort.service';
+import { ModulService } from './modul.service';
 
 export interface BildungsplanContextData {
   lernorte: Lernort[];
@@ -23,28 +23,28 @@ export interface BildungsplanContextData {
 export class BildungsplanContextService {
   private readonly hkbService = inject(HandlungskompetenzbereichService);
   private readonly hkService = inject(HandlungskompetenzService);
-  private readonly modulService = inject(ModulService);
   private readonly lernortService = inject(LernortService);
+  private readonly modulService = inject(ModulService);
 
-loadByEfz(efzId: number): Observable<BildungsplanContextData> {
-  return forkJoin({
-    lernorte: this.lernortService.getAll(),
-    handlungskompetenzbereiche: this.hkbService.getByEfzId(efzId),
-    handlungskompetenzen: this.hkService.getByEfzId(efzId),
-    module: this.modulService.getByEfzId(efzId),
-  });
-}
+  loadByEfz(efzId: number): Observable<BildungsplanContextData> {
+    return forkJoin({
+      lernorte: this.lernortService.getAll(),
+      handlungskompetenzbereiche: this.hkbService.getByEfzId(efzId),
+      handlungskompetenzen: this.hkService.getByEfzId(efzId),
+      module: this.modulService.getByEfzId(efzId),
+    });
+  }
 
-loadByFachrichtung(
-  fachrichtungId: number
-): Observable<BildungsplanContextData> {
-  return forkJoin({
-    lernorte: this.lernortService.getAll(),
-    handlungskompetenzbereiche:
-      this.hkbService.getByFachrichtungId(fachrichtungId),
-    handlungskompetenzen:
-      this.hkService.getByFachrichtungId(fachrichtungId),
-    module: this.modulService.getByFachrichtungId(fachrichtungId),
-  });
-} 
+  loadByFachrichtung(
+    fachrichtungId: number
+  ): Observable<BildungsplanContextData> {
+    return forkJoin({
+      lernorte: this.lernortService.getAll(),
+      handlungskompetenzbereiche:
+        this.hkbService.getByFachrichtungId(fachrichtungId),
+      handlungskompetenzen:
+        this.hkService.getByFachrichtungId(fachrichtungId),
+      module: this.modulService.getByFachrichtungId(fachrichtungId),
+    });
+  }
 }
